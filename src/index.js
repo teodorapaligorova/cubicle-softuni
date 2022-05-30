ca8fe1bead43cdbc98cc324fb90dec4dbe7992ad
Teodora <teodora.g.paligorova@gmail.com>
2022-05-31 00:01:53 +0300

    Rendered homepage
    Added handlebars
    Moved static to root folder
    Fixed mistakes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 app.use('/static', express.static('static'));
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views')
 
 app.get('/', (req,res)=>{
     res.send('');
