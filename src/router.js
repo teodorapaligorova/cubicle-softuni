28d1779662796e63ca7be5168e497b668e2e99ce
Teodora <teodora.g.paligorova@gmail.com>
2022-06-01 00:49:45 +0300

    created create cube router

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const homeController = require('./controllers/homeControler');
+const createController = require('./controllers/createController');
 
 const router = express.Router();
 
 router.get('/', homeController.index);
+router.use('/cube', createController);
 
 module.exports = router;
