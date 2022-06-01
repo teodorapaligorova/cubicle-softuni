4791e17cc6ffcf2437e68677b7de2bca3c946578
Teodora <teodora.g.paligorova@gmail.com>
2022-06-02 01:55:21 +0300

    created cube rendering

--- src/controllers/homeControler.js
+++ src/controllers/homeControler.js
@@ -1,7 +1,9 @@
 exports.index = ('/', (req,res)=>{
-    res.render('index');
+    res.render('index',{cubes});
 });
 
 exports.about = (req,res) => {
     res.render('about');
 }
+
+const cubes = require('../db.json')
