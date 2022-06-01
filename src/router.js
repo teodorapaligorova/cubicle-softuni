15b87aa9d644ab55eed5c04da48d4446c33aa0a8
Teodora <teodora.g.paligorova@gmail.com>
2022-06-02 01:28:12 +0300

    add about page

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ const createController = require('./controllers/createController');
 const router = express.Router();
 
 router.get('/', homeController.index);
+router.get('/about', homeController.about);
 router.use('/cube', createController);
 
 module.exports = router;
