3149f20ecc5d69f2f35b86d0857270f0d7758611
Teodora <teodora.g.paligorova@gmail.com>
2022-06-09 01:06:30 +0300

    add recourses for accesories

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -25,7 +25,7 @@ const cubeSchema = new mongoose.Schema({
 });
 
 cubeSchema.path('imageUrl').validate(function(){
-    return this.name.startsWith('http'); 
+    return this.imageUrl.startsWith('http'); 
 },'Image url shoul be a link')
 
 const Cube = mongoose.model('Cube', cubeSchema);
