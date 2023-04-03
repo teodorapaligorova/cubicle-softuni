b18e83fc87bad98dffa1ee6f28ce6816df80ae00
teodorapaligorova <teodora.g.paligorova@gmail.com>
2023-04-03 23:19:08 +0300

    Accessory validation and error handling

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,13 +1,45 @@
 const router = require('express').Router();
-const accessoryService = require('../services/accessoryService')
-
+const accessoryService = require('../services/accessoryService');
+const { getErrorMessage } = require('../utils/errorMapper');
+const { body, validationResult } = require('express-validator');
+const nameLength = 5;
+const minLength = 20;
+const maxLength = 150;
 
 router.get('/createAccessory', (req,res) => {
     res.render('accessory/createAccessory')
 });
 
-router.post('/createAccessory', async (req,res) =>{
-    await accessoryService.create(req.body)
-    res.redirect('/');
+router.post('/createAccessory',
+body("name").trim(),
+body("description").trim(),
+body("imageUrl").trim(),
+body("name")
+  .isLength({ min: nameLength })
+  .withMessage(`Cube name must be at least ${nameLength} characters long`)
+  .isAlphanumeric(['en-US'],{'ignore':' _-`'})
+  .withMessage("Accessory name must be numbers and letters only"),
+body("description")
+  .isLength({ min: minLength, max: maxLength })
+  .withMessage(`Desctiption must be from ${minLength} to ${maxLength} characters long`),
+body("imageUrl")
+  .isURL()
+  .withMessage("Image url should be a link"),
+async (req,res) =>{
+
+    const accessory = req.body;
+    const { errors } = validationResult(req);
+
+    try{
+        if (errors.length > 0) {
+            throw errors;
+          }
+        await accessoryService.create(accessory)
+        res.redirect('/');
+    }catch(error){
+        res.locals.errors = getErrorMessage(error);
+        res.render("accessory/createAccessory", {title: "Add Accessory", accessory })
+    }
+ 
 })
 module.exports = router;
--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -4,23 +4,14 @@ const accessorySchema = new mongoose.Schema({
     name:{
         type: String,
         required: true,
-        minlength: 5,
-        validate: /[a-zA-Z0-9 ]/
     },
     imageUrl: {
         type: String,
         required: true,
-        validate: {
-            validator: /^http?/,
-            message: 'Image url should be a link'
-        }
     },
     description: {
         type: String,
-        maxlength: 100,
-        minlength: 20,
         required: true,
-        validate: /[a-zA-Z0-9 ]/
     },
     cubes: [
     {
