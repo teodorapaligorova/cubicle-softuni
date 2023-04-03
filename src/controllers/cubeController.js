e93630545c2f1c3bc2dea04780ec8d8e28f2981d
teodorapaligorova <teodora.g.paligorova@gmail.com>
2023-04-03 21:56:49 +0300

    Cube edit functionalitis error handling

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,71 +1,154 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService');
-const accessoryService = require('../services/accessoryService')
+const accessoryService = require('../services/accessoryService');
+const { body, validationResult } = require('express-validator');
 const {isAuth} = require('../middlewares/userMiddleware');
-
+const { getErrorMessage } = require('../utils/errorMapper');
+const nameLength = 5;
+const minLength = 20;
+const maxLength = 150;
 
 router.get('/create',isAuth, (req,res) => {
     res.render('create')
 });
 
-router.post('/create', isAuth, (req,res) => {
+router.post('/create', isAuth, 
+body("name").trim(),
+body("description").trim(),
+body("imageUrl").trim(),
+body("name")
+  .isLength({ min: nameLength })
+  .withMessage(`Cube name must be at least ${nameLength} characters long`)
+  .isAlphanumeric(['en-US'],{'ignore':' _-'})
+  .withMessage("Cube name must be numbers and letters only"),
+body("description")
+  .isLength({ min: minLength, max: maxLength })
+  .withMessage(`Desctiption must be from ${minLength} to ${maxLength} characters long`),
+body("imageUrl")
+  .isURL()
+  .withMessage("Image url should be a link"),
+async (req,res) => {
+
     const cube = req.body;
     cube.owner = req.user._id;
+    const { errors } = validationResult(req);
 
-    if(cube.name.length < 2){
-        res.status(500).send('Invalid name');
-        return
-    }
+ try{
 
-    cubeService.create(cube)
-    .then(() => {
-        res.redirect('/')
-    })
-    .catch(err => {
-        res.status(400).send(err)
-    })
+    if (errors.length > 0) {
+        throw errors;
+      }
+
+    await cubeService.create(cube);
+    res.redirect('/')
+
+ }catch(error){
+    res.locals.errors = getErrorMessage(error);
+    res.render("create", {title: "Add Cube", cube: req.body })
+}
 });
 
 router.get('/details/:id', async(req,res) =>{
-    const cube = await cubeService.getOne(req.params.id).lean();
-    const isOwner = cube.owner == req.user?._id;
-    res.render('details', { cube, isOwner })
+
+    try{
+        const cube = await cubeService.getOne(req.params.id).lean();
+        const isOwner = cube.owner == req.user?._id;
+        res.render('details', { cube, isOwner }) 
+    }catch(error){
+        res.locals.errors = getErrorMessage(error);
+        res.redirect('404');
+    }
+
 });
 
 router.get('/:cubeId/attach-accessory', async (req,res) => {
-    const cube = await cubeService.getOne(req.params.cubeId).lean();
-    const accessories = await accessoryService.getAll().lean()
-    res.render('accessory/attachAccessory',{cube, accessories});
+
+    try{
+        const cube = await cubeService.getOne(req.params.cubeId).lean();
+        const accessories = await accessoryService.getAll().lean()
+        res.render('accessory/attachAccessory',{cube, accessories});
+    }catch(error){
+        res.locals.errors = getErrorMessage(error);
+        res.redirect('404');
+    }
+  
 });
 
 router.post('/:cubeId/attach-accessory', async (req,res) => {
+
     const accessoryId = req.body.accessory;
 
-    await cubeService.attachAccessory(req.params.cubeId, accessoryId)
+    try{
+        await cubeService.attachAccessory(req.params.cubeId, accessoryId);
+        res.redirect(`/cube/details/${req.params.cubeId}`);
 
-    res.redirect(`/cube/details/${req.params.cubeId}`)
+    }catch(error){
+        res.locals.errors = getErrorMessage(error);
+        res.render('accessory/attachAccessory', {title: "Atach Accessory"})
+    }
+
+   
 });
 
 router.get('/:cubeId/edit', isAuth, async(req, res) => {
-    const cube = await cubeService.getOne(req.params.cubeId).lean();
 
-    if(cube.owner != req.user._id) {
-        
-        return res.redirect('404');
-    }
+    try{
 
-    if(!cube){
-        return res.redirect('404')
+        const cube = await cubeService.getOne(req.params.cubeId).lean();
+
+        if(cube.owner != req.user._id) {
+            
+            return res.redirect('404');
+        }
+
+        if(!cube){
+            return res.redirect('404');
+        }
+
+        cube[`difficultyLevel${cube.difficultyLevel}`] = true;
+        res.render('editCube', {title: 'Edit Cube', cube });
+
+    }catch(error){
+        res.locals.errors = getErrorMessage(error);
     }
-    res.render('editCube', { cube })
+  
+
 });
 
-router.post('/:cubeId/edit', async (req, res) => {
-    let editedCube = await cubeService.edit(req.params.cubeId, req.body)
-    res.redirect(`/cube/details/${editedCube._id}`)
+router.post('/:cubeId/edit',
+body("name").trim(),
+body("description").trim(),
+body("imageUrl").trim(),
+body("name")
+  .isLength({ min: nameLength })
+  .withMessage(`Cube name must be at least ${nameLength} characters long`)
+  .isAlphanumeric(['en-US'],{'ignore':' _-'})
+  .withMessage("Cube name must be numbers and letters only"),
+body("description")
+  .isLength({ min: minLength, max: maxLength })
+  .withMessage(`Desctiption must be from ${minLength} to ${maxLength} characters long`),
+body("imageUrl")
+  .isURL()
+  .withMessage("Image url should be a link"),
+async (req, res) => {
+
+    const { errors } = validationResult(req);
+
+    try{
+        if (errors.length > 0) {
+            throw errors;
+          }
+    
+        let editedCube = await cubeService.edit(req.params.cubeId, req.body);
+        res.redirect(`/cube/details/${editedCube._id}`);
+    }catch(error){
+        res.locals.errors = getErrorMessage(error);
+        res.render('editCube', { title: 'Edit', cube: req.body });
+    }
+    
 });
 
-router.get('/:cubeId/delete', async(req,res) => {
+router.get('/:cubeId/delete', async(req,res) => {u
     const cube = await cubeService.getOne(req.params.cubeId).lean();
 
     res.render('cube/delete', { cube });
